ca65a5fb2d6b208db052ed0a26eaf9b77b3ae667
Hiren Vavadiya <vavadiyahiren@gmail.com>
2022-01-05 17:26:02 +0530

    # # added message model from plugin to transfer message (added missing class)

---
+++ lib/message_event.dart
@@ -0,0 +1,58 @@
+class MessageEvent {
+  String? customText;
+  String? from;
+  String? senderJid;
+  int? time;
+  String? id;
+  String? type;
+  String? body;
+  String? msgtype;
+  String? bubbleType;
+  String? mediaURL;
+  String? presence;
+
+  MessageEvent({
+    this.customText,
+    this.from,
+    this.senderJid,
+    this.time,
+    this.id,
+    this.type,
+    this.body,
+    this.msgtype,
+    this.bubbleType,
+    this.mediaURL,
+    this.presence,
+  });
+
+  Map<String, dynamic> toEventData() {
+    return {
+      'customText': customText,
+      'from': from,
+      'senderJid': senderJid,
+      'time': time,
+      'id': id,
+      'type': type,
+      'body': body,
+      'msgtype': msgtype,
+      'bubbleType': bubbleType,
+      'mediaURL': mediaURL,
+    };
+  }
+
+  factory MessageEvent.fromJson(dynamic eventData) {
+    return MessageEvent(
+      customText: eventData['customText'] ?? '',
+      from: eventData['from'] ?? '',
+      senderJid: eventData['senderJid'] ?? '',
+      time: eventData['time'] ?? 0,
+      id: eventData['id'] ?? '',
+      type: eventData['type'] ?? '',
+      body: eventData['body'] ?? '',
+      msgtype: eventData['msgtype'] ?? '',
+      bubbleType: eventData['bubbleType'] ?? '',
+      mediaURL: eventData['mediaURL'] ?? '',
+      presence: eventData['presence'] ?? '',
+    );
+  }
+}
