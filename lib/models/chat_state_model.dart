2bf71fba68afbb142aa8c40cffad516eabd2773f
fedehsq <fedebernacca@gmail.com>
2024-05-31 01:08:34 +0200

    fix 'ChatState.fromJson' and changed dart 'TypingStatus' toLower

--- lib/models/chat_state_model.dart
+++ lib/models/chat_state_model.dart
@@ -1,4 +1,5 @@
 import '../ennums/typing_status.dart';
+import 'package:collection/collection.dart';
 
 class ChatState {
   String? from;
@@ -35,7 +36,8 @@ class ChatState {
       id: eventData['id'] ?? '',
       type: eventData['type'] ?? '',
       msgtype: eventData['msgtype'] ?? '',
-      chatStateType: eventData['chatStateType'] ?? '',
+      chatStateType: TypingStatus.values.firstWhereOrNull(
+              (e) => e.name == (eventData['chatStateType'] ?? ''))
     );
   }
 }
